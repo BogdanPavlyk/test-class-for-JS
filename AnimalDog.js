3189bad00a54014a70420bfc694389ff0574b0b9
BogdanPavlyk <bogdan.pavlyk7@gmail.com>
2016-01-29 17:07:04 +0200

    Added JavaScript file with two classes: class Animal with method getName(), and class Dog that extends Animal and with own method bark().

---
+++ AnimalDog.js
@@ -0,0 +1,29 @@
+﻿"use strict";
+
+// 1. Конструктор Animal
+function Animal(name) {
+  this.name = name;
+}
+
+// 1.1. Методы -- в прототип
+Animal.prototype.getName = function() {
+  return this.name;
+}
+
+// 2. Конструктор Dog
+function Dog(name) {
+  Animal.apply(this, arguments);
+}
+
+// 2.1. Наследование
+Dog.prototype = Object.create(Animal.prototype);
+Dog.prototype.constructor = Dog;
+
+// 2.2. Методы Dog
+Dog.prototype.bark = function() {
+  return 'Dog '+ this.name + ' is barking';
+}
+
+var dog = new Dog ('Aban');
+alert(dog.getName() === 'Aban');
+alert(dog.bark() === 'Dog Aban is barking');
